48ca1c74e5cbc545e03d7eb28b5b0c636120cf31
izzybogart <60237226+izzybogart@users.noreply.github.com>
2021-02-05 09:34:37 -0700

    Finished Merge-Sort Program
    Fixed for loop issues and made iterations of each sort print.

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -2,35 +2,55 @@
 #include <math.h>
 using namespace std;
 
-//Merge
+//Print Array function
+void printArr(int A[], int length){
+	for(int i = 0; i < length; i++)
+		cout<<A[i]<<" ";
+	cout<<endl;
+}
 
+//Merge
 void merge(int A[], int p, int q, int r){
-	//initialize the size of the left and right array
+//initialize the size of the left and right array
 	int leftSz = q - p + 1;
 	int rightSz = r - q;
-	
-	int Left[leftSz + 1], Right[rightSz + 1];
-	//divide the values of A into the left and right arrays
-	for(int i = 0; i <= leftSz; i++)
-		Left[i] = A[p + i - 1];
 
-	for(int j = 0; j <= rightSz; j++)
+	int Left[leftSz], Right[rightSz];
+//divide the values of A into the left and right arrays
+	for(int i = 0; i < leftSz; i++){
+		Left[i] = A[p + i - 1];
+	}
+
+	for(int j = 0; j < rightSz; j++){
 		Right[j] = A[q + j];
-	//since my program only generates random numbers between 0 and 100, 101 will work as infinity
-	Left[leftSz + 1] = 101;
-	Right[rightSz + 1] = 101;
-	
-	int i = 1;
-	int j = 1;
-	for(int k = p; k <= r; k++){
-		if(Left[i] <= Right[j]){
-			A[k] = Left[i];
-			i++;
-		}
-		else{
-			A[k] = Right[j];
-			j++;
-		}
+	}
+
+	int i = 0;
+	int j = 0;
+	int k = p - 1;
+
+//sorts elements of Right and Left back into A
+	while (i < leftSz && j < rightSz){
+        if(Left[i] <= Right[j]){
+            A[k] = Left[i];
+            i++;
+        }
+        else{
+            A[k] = Right[j];
+            j++;
+        }
+        k++;
+	}
+//Copies remaining elements of Left and Right into A (if any)
+	while(i < leftSz){
+        A[k] = Left[i];
+        i++;
+        k++;
+	}
+	while(j < rightSz){
+        A[k] = Right[j];
+        j++;
+        k++;
 	}
 }
 
@@ -40,16 +60,11 @@ void mergeSort(int A[], int p, int r){
 		int var = (p + r)/2;
 		int q = floor(var);
 		mergeSort(A, p, q);
+		printArr(A,r);
 		mergeSort(A, q + 1, r);
+		printArr(A,r);
 		merge(A, p, q, r);
 	}
-}
-
-//Print Array function
-void printArr(int A[], int length){
-	for(int i = 0; i < length; i++)
-		cout<<A[i]<<" ";
-	cout<<endl;
 }
 
 int main() {
@@ -59,13 +74,14 @@ int main() {
 		A[i] = rand() % 100;
 
 	int lengthA = sizeof(A)/sizeof(A[0]);
-	
-	cout<<"Unsorted Array"<<endl;
+
+	cout<<" Unsorted Array"<<endl;
 	printArr(A, lengthA);
-	
+
+	cout<<"\n Merge-Sort Iterations"<<endl;
 	mergeSort(A, 1, lengthA);
 
-	cout<<"Sorted Array"<<endl;
+	cout<<"\n Sorted Array"<<endl;
 	printArr(A, lengthA);
 
 	return 0;
